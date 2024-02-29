300249fcf2820bb0d142ba6906031894b7751c83
Dexter Hoang <dhhoang@ucsc.edu>
2024-02-28 20:46:45 -0800

    added peter griffin

--- src/scenes/Title.js
+++ src/scenes/Title.js
@@ -16,15 +16,18 @@ class Title extends Phaser.Scene {
         this.load.image('minerva', 'img/minerva.png')
         this.load.image('jove', 'img/jove.png')
         this.load.image('neptune', 'img/neptune.png')
+        this.load.image('peter', 'img/peter.png')
 
         // load bitmap font
         this.load.bitmapFont('gem_font', 'font/gem.png', 'font/gem.xml')
+		this.load.bitmapFont('mickey_font', 'font/mickey.png', 'font/mickey.xml')
+	
     }
 
     create() {
         // add title text
-        this.add.bitmapText(centerX, centerY - 32, 'gem_font', 'THE ODYSSEY', 32).setOrigin(0.5)
-        this.add.bitmapText(centerX, centerY, 'gem_font', 'Press SPACE to start', 16).setOrigin(0.5)
+        this.add.bitmapText(centerX, centerY - 32, 'mickey_font', 'THE ODYSSEY', 72).setOrigin(0.5)
+        this.add.bitmapText(centerX, centerY, 'mickey_font', 'Press SPACE to start', 32).setOrigin(0.5)
 
         // create input
         cursors = this.input.keyboard.createCursorKeys()
